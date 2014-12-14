18b46ff2e82bd6c1c4b6883fd04d54a190b73090
motoy3d <motoy3d@gmail.com>
2014-12-15 04:03:14 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'ardija'
+    ,hashtag: 'ardija'
+    ,teamName: '大宮'
+    ,appName: 'スマートJ for 大宮アルディージャ'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041396?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.ardija'
+    ,color: '#ed6d02'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.ardija'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=ardija&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=ardija"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 249106
+    ,nendApiKeyIPhoneIcon: '42e659bd0d9c9cb8b994b5ec88d757b2ffec1db0'
+    ,nendSpotIdIPhoneBanner: 249105
+    ,nendApiKeyIPhoneBanner: '0f1a89dd46725a2b452af85060ca126b92ef6410'
+    //nend(Android)
+    ,nendSpotIdAndroid: 249104
+    ,nendApiKeyAndroid: '954bb7bf8b9fff349d87b77fb24ffd7481c5c476'
+};
