dc3c119ba9d4af8d559714b78a9e34e0ec9b42b2
motoy3d <motoy3d@gmail.com>
2017-07-02 11:36:08 +0900

    iOS1.0.1iOS10対応。他チーム情報機能。1ステージ対応。

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'ardija'
     ,hashtag: 'ardija'
     ,teamName: '大宮'
+    ,teamNameFull: '大宮アルディージャ'
     ,appName: 'スマートJ for 大宮アルディージャ'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041396?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.ardija'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#ddd'
+    ,mainTextColor: 'white'
     ,color: '#ed6d02'
     ,tintColor: 'white'
+    ,standingsBgcolor: '#ed6d02'
+    ,standingsTeamFontColor: 'white'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'smartj.ardija'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
-    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    ,feedUrlBase: 'https://smartj.mobi/redsmylife/news.json'
     //日程・結果のソースURL
-    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=ardija&season="
+    ,resultsUrl: "https://smartj.mobi/redsmylife/results.json?teamId=ardija&season="
+    //順位表のソースURL
+    ,standingsUrl: 'https://smartj.mobi/redsmylife/standings.json'
     //設定、メッセージのソースURL
-    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=ardija"
+    ,messageUrl: "https://smartj.mobi/redsmylife/config.json?teamId=ardija"
+    //動画のソースURL
+    ,videoUrl: "https://smartj.mobi/redsmylife/video.json?teamId=ardija"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=antlers"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
@@ -24,4 +46,6 @@ exports.config = {
     //nend(Android)
     ,nendSpotIdAndroid: 249104
     ,nendApiKeyAndroid: '954bb7bf8b9fff349d87b77fb24ffd7481c5c476'
+    ,nendSpotIdAndroidBanner: 393953
+    ,nendApiKeyAndroidBanner: '31c63f4d90b65a6bb762c13a1328f00e9da6705b'
 };
